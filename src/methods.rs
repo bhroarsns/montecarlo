bb2e5230701474dcabf79f44feb6f71f52ce73cc
bhroarnsns2 <dixneufbhroarsns3mfcm@gmail.com>
2023-05-15 18:02:33 +0900

    version

--- src/methods.rs
+++ src/methods.rs
@@ -3,13 +3,10 @@ pub trait MetropolisHastings {
     fn acceptance_probability(&self, state: &Self::State) -> f64;
     fn candidate_state(&self) -> Self::State;
     fn generate_random_number(&mut self) -> f64;
-    fn get_next_state(&mut self) -> Self::State {
+    fn get_next_state(&mut self) {
         let candidate = self.candidate_state();
         if self.generate_random_number() < self.acceptance_probability(&candidate) {
             self.set_state(&candidate);
-            candidate
-        } else {
-            self.state()
         }
     }
     fn set_state(&mut self, state: &Self::State);
